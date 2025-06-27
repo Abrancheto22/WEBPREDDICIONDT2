244a5c28eaf987b24c85894320c9e3c8929ea228
Abrancheto22 <abraham_benj18@hotmail.com>
2025-06-27 18:36:37 -0500

    conexion base de datos

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\IndexController;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', [IndexController::class, 'index'])->name('index');
